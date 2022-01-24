76caf6e18da4ab9fecb92d8649853d1b94f9fcc1
Nicolas Glassey <nicolas.glassey@altis.swiss>
2022-01-24 17:57:24 +0100

    Added fields

--- edelblack-mqtt-panel/src/module.ts
+++ edelblack-mqtt-panel/src/module.ts
@@ -5,15 +5,43 @@ import { SimplePanel } from './SimplePanel';
 export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
   return builder
     .addTextInput({
-      path: 'text',
-      name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
+      path: 'mqttServ',
+      name: 'Server',
+      description: 'WebSocket endpoint for the MQTT server',
+      defaultValue: 'wss://localhost:9001/wss',
+    })
+    .addTextInput({
+      path: 'listenTopic',
+      name: 'Listen topic',
+      description: 'Topic to listen to',
+      defaultValue: 'edelblack/mqtt/panel/listen'
+    })
+    .addTextInput({
+      path: 'publishTopic',
+      name: 'Publish topic',
+      description: 'Topic where the data will be published',
+      defaultValue: 'edelblack/mqtt/panel/publish'
     })
     .addBooleanSwitch({
-      path: 'showSeriesCount',
-      name: 'Show series counter',
+      path: 'publishRetain',
+      name: 'Retain',
+      description: 'Send the data as retained',
       defaultValue: false,
+    })
+    .addNumberInput({
+      path: 'publishQos',
+      name: 'QOS',
+      description: 'QOS for the sent data',
+      defaultValue: 0,
+      min: 0,
+      max: 2,
+      step: 1,
+      integer: true
+    })
+    .addBooleanSwitch({
+      path: 'waitBeforePublish',
+      name: 'Prevent publishing until data arrived',
+      defaultValue: true,
     })
     .addRadio({
       path: 'seriesCountSize',
